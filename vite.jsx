975867ac1c7eae04888c9253f77116b92416d604
satyam-jais1233 <satyajaiswal00001@gmail.com>
2025-09-09 15:30:25 +0530

    Updates

---
+++ vite.jsx
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'netflix-project', // 👈 must match your GitHub repo name exactly
+})
