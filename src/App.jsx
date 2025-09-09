94fe24bb4f64c63af8f73171b6f1b8c196f86452
satyam-jais1233 <satyajaiswal00001@gmail.com>
2025-09-09 15:35:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
+// Components
 import Header from "./assets/components/header";
 import Banner from "./assets/components/homebanner";
 import Login from "./assets/components/login";
@@ -12,32 +12,37 @@ import Card from "./assets/components/card";
 import Accordion from "./assets/components/list";
 import Restart from "./assets/components/last";
 import Footer from "./assets/components/footer";
+
 function App() {
   const [count, setCount] = useState(0);
+  
 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Banner />
-                <MovieList />
-                <Card />
-                <Accordion />
-                <Restart />
-                <Footer />
-              </>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Banner />
+              <MovieList />
+              <Card />
+              <Accordion />
+              <Restart />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* Login Page */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
