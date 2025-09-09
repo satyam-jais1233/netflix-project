f9e0297def5d48ef6e2b81227c6b40d8bfcd1d0a
satyam-jais1233 <satyajaiswal00001@gmail.com>
2025-09-09 13:20:49 +0530

    second update

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from "./assets/react.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./assets/components/header";
 import Banner from "./assets/components/homebanner";
 import Login from "./assets/components/login";
